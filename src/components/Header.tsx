9c83f3aa854eb4e14c44477c1439feec5b589e3e
jodosjodos <jeandedieu2030@gmail.com>
2024-02-08 09:36:30 +0200

    change content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,8 @@ const Header = () => {
       </button>
       <div className="flex flex-col gap-1  justify-center items-center border-b-2 border-secondary border-dashed py-6 px-4 leading-tight">
         <p>
-          Welcome to Ubuhanga group where a dedicated team of professions works{" "}
+          Welcome to [Your Company Name] group where a dedicated team of
+          professions works
         </p>
         <p>
           together to bring innovation , creativity and expertise to software
